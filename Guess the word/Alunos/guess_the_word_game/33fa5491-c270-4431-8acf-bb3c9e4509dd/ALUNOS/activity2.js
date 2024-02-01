a187eda64305759db4bf2431f0b0a6d82becaf0a
MiguelBB123 <148267617+MiguelBB123@users.noreply.github.com>
2024-02-01 11:19:21 -0300

    Add files via upload

---
+++ Guess the word/Alunos/guess_the_word_game/33fa5491-c270-4431-8acf-bb3c9e4509dd/ALUNOS/activity2.js
@@ -0,0 +1,7 @@
+function back(){
+    window.location = "index.html";
+}
+function getScore(){
+    score = localStorage.getItem("score");
+    document.getElementById("update").innerHTML = "<h1> Pontuação: "+ score + "</h1>";
+}
